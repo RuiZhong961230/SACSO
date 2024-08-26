3ef2d26509c654b1fc0988a682f06e13b038534f
RuiZhong <rui.zhong.u5@elms.hokudai.ac.jp>
2024-08-26 20:10:04 +0800

    First Commit

---
+++ SACSO/SACSO_HPA.py
@@ -0,0 +1,158 @@
+# import packages
+import os
+from hpa.problem import HPA101, HPA102, HPA103, HPA131, HPA142, HPA143
+import numpy as np
+from copy import deepcopy
+
+
+DimSize = 100
+PopSizeMax = 40
+PopSizeMin = 4
+PopSize = PopSizeMax
+LB = [-100] * DimSize
+UB = [100] * DimSize
+TrialRuns = 20
+MaxFEs = 1000
+
+Pop = np.zeros((PopSize, DimSize))
+Velocity = np.zeros((PopSize, DimSize))
+FitPop = np.zeros(PopSize)
+curFEs = 0
+FuncNum = 1
+
+
+# initialize the M randomly
+def Initialization(func):
+    global Pop, Velocity, FitPop, PopSize, PopSizeMax, curFEs
+    PopSize = PopSizeMax
+    Pop = np.zeros((PopSize, DimSize))
+    FitPop = np.zeros(PopSize)
+    Velocity = np.zeros((PopSize, DimSize))
+    for i in range(PopSize):
+        for j in range(DimSize):
+            Pop[i][j] = LB[j] + (UB[j] - LB[j]) * np.random.rand()
+        FitPop[i] = func(Pop[i])
+        curFEs += 1
+
+
+def Check(indi):
+    global LB, UB
+    for i in range(DimSize):
+        range_width = UB[i] - LB[i]
+        if indi[i] > UB[i]:
+            n = int((indi[i] - UB[i]) / range_width)
+            mirrorRange = (indi[i] - UB[i]) - (n * range_width)
+            indi[i] = UB[i] - mirrorRange
+        elif indi[i] < LB[i]:
+            n = int((LB[i] - indi[i]) / range_width)
+            mirrorRange = (LB[i] - indi[i]) - (n * range_width)
+            indi[i] = LB[i] + mirrorRange
+        else:
+            pass
+    return indi
+
+
+def SACSO(func):
+    global Pop, Velocity, FitPop, PopSize, curFEs
+
+    tmp = sorted(list(zip(Pop, FitPop)), key=lambda x: x[1])
+    phi_list = np.random.uniform(0.001, 0.5, PopSize)
+    Pop, FitPop = zip(*tmp)
+
+    sequence = list(range(PopSize))
+    np.random.shuffle(sequence)
+    Off = np.zeros((PopSize, DimSize))
+    FitOff = np.zeros(PopSize)
+    Xmean = np.mean(Pop, axis=0)
+
+    for i in range(int(PopSize / 2)):
+        idx1 = sequence[2 * i]
+        idx2 = sequence[2 * i + 1]
+
+        if FitPop[idx1] < FitPop[idx2]:
+            Off[idx1] = deepcopy(Pop[idx1])
+            FitOff[idx1] = FitPop[idx1]
+            phi = phi_list[idx2]
+            Velocity[idx2] = np.random.rand(DimSize) * Velocity[idx2] + np.random.rand(DimSize) * (Pop[idx1] - Pop[idx2]) + phi * (Xmean - Pop[idx2])
+            Off[idx2] = Pop[idx2] + Velocity[idx2]
+            Off[idx2] = Check(Off[idx2])
+            FitOff[idx2] = func(Off[idx2])
+            curFEs += 1
+        else:
+            Off[idx2] = deepcopy(Pop[idx2])
+            FitOff[idx2] = FitPop[idx2]
+            phi = phi_list[idx1]
+
+            Velocity[idx1] = np.random.rand(DimSize) * Velocity[idx1] + np.random.rand(DimSize) * (Pop[idx2] - Pop[idx1]) + phi * (Xmean - Pop[idx1])
+            Off[idx1] = Pop[idx1] + Velocity[idx1]
+            Off[idx1] = Check(Off[idx1])
+            FitOff[idx1] = func(Off[idx1])
+            curFEs += 1
+
+    PopSize = round(((PopSizeMin - PopSizeMax) / MaxFEs * curFEs + PopSizeMax))
+    if PopSize % 2 == 1:
+        PopSize += 1
+
+    PopSize = max(PopSize, PopSizeMin)
+    sorted_idx = np.argsort(FitOff)
+    Pop = deepcopy(Off[sorted_idx[0:PopSize]])
+    FitPop = deepcopy(FitOff[sorted_idx[0:PopSize]])
+
+
+def RunSACSO(func):
+    global FitPop, curFEs, TrialRuns, DimSize
+    All_Trial = []
+    for i in range(TrialRuns):
+        BestList = []
+        curFEs = 0
+        np.random.seed(2024 + 23 * i)
+        Initialization(func)
+        BestList.append(min(FitPop))
+        while curFEs < MaxFEs:
+            SACSO(func)
+            BestList.append(min(FitPop))
+        All_Trial.append(BestList)
+    return All_Trial
+
+
+def mainCons(ndiv, level):
+    global FuncNum, DimSize, Pop, MaxFEs, curIter, MaxIter, LB, UB
+    Probs = [HPA131(n_div=ndiv, level=level), HPA142(n_div=ndiv, level=level), HPA143(n_div=ndiv, level=level)]
+    Name = ["HPA131", "HPA142", "HPA143"]
+    for i in range(len(Probs)):
+        DimSize = Probs[i].nx
+        Pop = np.zeros((PopSize, DimSize))
+        MaxFEs = 1300
+        MaxIter = int(MaxFEs / PopSize)
+        LB = [0] * DimSize
+        UB = [1] * DimSize
+        FuncNum = Name[i]
+        Trace = RunSACSO(Probs[i])
+        np.savetxt("./SACSO_Data/HPA_Cons/" + FuncNum + "_" + str(ndiv) + "_" + str(level) + ".csv", Trace, delimiter=",")
+
+
+def mainUncons(ndiv, level):
+    global FuncNum, DimSize, Pop, MaxFEs, curIter, MaxIter, LB, UB
+    Probs = [HPA101(n_div=ndiv, level=level), HPA102(n_div=ndiv, level=level), HPA103(n_div=ndiv, level=level)]
+    Name = ["HPA101", "HPA102", "HPA103"]
+    for i in range(len(Probs)):
+        DimSize = Probs[i].nx
+        Pop = np.zeros((PopSize, DimSize))
+        MaxFEs = 1000
+        MaxIter = int(MaxFEs / PopSize)
+        LB = [0] * DimSize
+        UB = [1] * DimSize
+        FuncNum = Name[i]
+        Trace = RunSACSO(Probs[i])
+        np.savetxt("./SACSO_Data/HPA_Uncons/" + FuncNum + "_" + str(ndiv) + "_" + str(level) + ".csv", Trace, delimiter=",")
+
+
+if __name__ == "__main__":
+    if os.path.exists('./SACSO_Data/HPA_Uncons') == False:
+        os.makedirs('./SACSO_Data/HPA_Uncons')
+    if os.path.exists('./SACSO_Data/HPA_Cons') == False:
+        os.makedirs('./SACSO_Data/HPA_Cons')
+    for ndiv in range(4, 6):
+        for level in range(0, 2):
+            mainUncons(ndiv, level)
+            mainCons(ndiv, level)
